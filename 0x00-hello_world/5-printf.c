c0367dd8cb841c9b794b5a6c27abccfec6335b4a
oumaimabouhila <bouhilaoumaima@gmail.com>
2023-05-18 15:57:31 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - A progress that prints a line with puts function
- * return: 0 (success)
- */
-
-int main (void)
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
