55d1135a23fdebc1875af1f411c249062d200927
Queengirl18 <bouhilaoumaima@gmail.com>
2023-11-03 12:37:12 -0700

    read

--- 0x15-file_io/0-main.c
+++ 0x15-file_io/0-main.c
@@ -4,23 +4,23 @@
 
 /**
  * main - check the code
- *ac: argument
- *av: argument
+ *argc: argument change 
+ *argv: argument
  *
  * Return: Always 0.
  */
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
 ssize_t n;
 
-if (ac != 2)
+if (argc != 2)
 {
-dprintf(2, "Usage: %s filename\n", av[0]);
+dprintf(2, "Usage: %s filename\n", argv[0]);
 exit(1);
 }
-n = read_textfile(av[1], 114);
+n = read_textfile(argv[1], 114);
 printf("\n(printed chars: %li)\n", n);
-n = read_textfile(av[1], 1024);
+n = read_textfile(argv[1], 1024);
 printf("\n(printed chars: %li)\n", n);
 return (0);
 }
